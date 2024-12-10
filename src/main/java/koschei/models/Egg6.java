902ba715e50e28a657f2b332decb8d6072f80303
Vhite128 <vhite@internet.ru>
2024-12-10 07:34:47 +0300

    12.24

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,7 +1,6 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 public class Egg6 {
     private final Needle7 needle;
